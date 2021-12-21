c48a5cd76d63feeab0a1bb0e43733e84bc0893dd
NikitaLekomtsev <90478242+NikitaBlitz@users.noreply.github.com>
2021-12-21 09:41:47 +0500

    Add files via upload

---
+++ Algoritm/equations_Iterations.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+// f(x) = x^3 - x - 1
+// f'(x) = 3x^2 - 1
+// f''(x) = 6x;
+// [1, 2]
+// ф(x) = x + λ(x^3 - x - 1)
+// -1/r < λ < 0, если f'(x) > 0
+// 0 < λ < 1/r, если f'(x) < 0
+// r = max(|f'(a)|, |f'(b)|)
+// λ = -1/20 = -0.05
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float x, xPrev;
+    double lambda = -0.05; // -1/20
+    x = 2; // можно выбрать любое x0 из [1, 2] — выбранного заранее промежутка
+    xPrev = 0; //делаем заведомо сильно меньше x, чтобы цикл правильно сработал
+    double eps = 0.000001;
+
+    while (abs(x - xPrev) > eps)
+    {
+        xPrev = x;
+        x = lambda * (pow(xPrev, 3) - xPrev - 1) + xPrev;
+    }
+    cout << "Корень по методу итераций: " << x << endl;
+    return 0;
+}
---
+++ Algoritm/equations_Newton.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+// f(x) = x^3 - x - 1
+// f'(x) = 3x^2 - 1
+// f''(x) = 6x;
+// [1, 2]
+// f(b) * f''(b) > 0
+// 8 - 2 - 1 * 6*2 > 0 => x0 = b;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float x, xPrev;
+    // f(b) * f''(b) > 0
+    // 8 - 2 - 1 * 6*2 > 0 => x0 = b;
+    x = 2;
+    xPrev = 0;
+    double eps = 0.000001;
+
+    while (abs(x - xPrev) > eps)
+    {
+        xPrev = x;
+        x = xPrev - ((pow(xPrev, 3) - xPrev - 1) / (3 * pow(xPrev, 2) - 1));
+    }
+    cout << "Корень: " << x << endl;
+    return 0;
+}
